4724b051df437c0f31349ec7dc229ad7f291394d
asish-dewan <104346122+asish-dewan@users.noreply.github.com>
2022-11-07 23:13:00 +1100

    Added inquirer prompts

--- server.js
+++ server.js
@@ -21,21 +21,49 @@ db.connect (err => {
 });
 
 function employee_tracker() {
-    inquirer.prompt ({
-        type: 'list',
-        name: 'home',
-        message: 'What would you like to do?',
-        choices: ['View all departments', 
-        'View all roles', 
-        'View all employees', 
-        'Add a role', 
-        'Add an employee', 
-        'Update an employee', 
-        'Exit']
-    })
-    .then (
-        ({home}) => {
-            
+    inquirer
+    .prompt([
+    {
+        type: "list",
+        name: "userChoice",
+        message: "What would you like to do?",
+        choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update an employee role",
+        ],
+    },
+    ])
+    .then((answers) => {
+        console.log(answers);
+        if (answers.userChoice === "View all departments") {
+            viewDepartments();
+            //presented with a formatted table showing department names and department ids
         }
-    )
-}
+        if (answers.userChoice === "View all roles") {
+            viewRoles();
+            //presented with the job title, role id, the department that role belongs to, and the salary for that role
+        }
+        if (answers.userChoice === "View all employees") {
+            viewEmployees();
+            //presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+        }
+        if (answers.userChoice === "Add a department") {
+            addDepartment();
+        }
+        if (answers.userChoice === "Add a role") {
+            addRole();
+        }
+        if (answers.userChoice === "Add an employee") {
+            addEmployee();
+        }
+        if (answers.userChoice === "Update an employee role") {
+            updateEmployeeRole();
+        }
+        });
+    };
+
